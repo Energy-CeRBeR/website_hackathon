05e8a171adbfde58c6cb309a044c95d1f961e744
admin5225 <f89155961442@yandex.ru>
2024-04-27 21:16:27 +0300

    yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Voting from "./Components/Voting/Voting"
 import { voiting_data } from "./data"
 
 function App() {
+  const districts = fetch('')
+
   return (
     <>
       <Voting data={voiting_data}/>
