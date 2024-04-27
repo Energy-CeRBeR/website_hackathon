94ce4c8079428213f65ea3deb91655a64282cc91
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-04-27 21:31:19 +0300

    Create CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.routers import router as auth_router
 from src.routes.rating_routers import router as rating_router
 
 app = FastAPI(
     title="Tracker Service"
+)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
