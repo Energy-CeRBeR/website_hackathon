6592a066202ee985bc8d8df4d4d55d91417624f3
1_ace_1 <yuribaryschev@yandex.ru>
2024-04-27 21:40:45 +0300

    mn

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,6 +15,7 @@ origins = [
     "http://localhost:8080",
     "http://localhost:8000",
     "http://localhost:5173",
+    "http://localhost:5174"
 ]
 
 app.add_middleware(
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Voting from "./Components/Voting/Voting"
 import { voiting_data } from "./data"
 
 function App() {
-  const districts = fetch('http://127.0.0.1:8000/ratings/').json()
+  const districts = fetch('http://127.0.0.1:8000/ratings/')
   console.log(districts)
 
   return (
