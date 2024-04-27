8f14d8f44baf140791b4fa63925d813c96fe1cc8
admin5225 <f89155961442@yandex.ru>
2024-04-27 21:19:06 +0300

    no

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import Voting from "./Components/Voting/Voting"
 import { voiting_data } from "./data"
 
 function App() {
-  const districts = fetch('')
+  const districts = fetch('http://127.0.0.1:8000/ratings/').json()
+  console.log(districts)
 
   return (
     <>
