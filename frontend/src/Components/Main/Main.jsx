8904b90b0a81320d01fc89536ebc4b3b109918ec
admin5225 <f89155961442@yandex.ru>
2024-04-27 13:35:54 +0300

    norm

---
+++ frontend/src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css'
+
+import List from '../List/List'
+
+import {background_information} from '../../data'
+
+export default function Main () {
+    return (
+        <div className='background_information'>
+            {background_information.map((dict) => (<List key={Object.keys(dict)[0]} dict={dict}/>))}
+        </div>
+    )
+}
