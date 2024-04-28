1ffe7ad265fab29ec09ef66362b39f55aca00bc1
Mitya <dim.maluckow2017@yandex.ru>
2024-04-28 04:23:15 +0300

    Link Back and Front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import RegistrationForm from "./Components/RegistrationForm/RegistrationForm"
 import EntranceForm from "./Components/RegistrationForm/EntranceForm"
 import Voting from "./Components/Voting/Voting"
+import axios from "axios";
+import {useState, useEffect} from 'react'
 
 import { voiting_data } from "./data"
 
 function App() {
-  const districts = fetch('http://127.0.0.1:8000/ratings/')
-  console.log(districts)
+  const [districts, setDistricts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/ratings/').then(r => {
+      setDistricts(r.data);
+    });
+  }, []);
 
   return (
     <>
-      <Voting data={voiting_data}/>
+      <Voting data={districts} />
     </>
-  )
-}
+  )}
 
 export default App
