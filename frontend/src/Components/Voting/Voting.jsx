7d2f12af2c7dbfe97b93de4c6f31191345cfc22a
admin5225 <f89155961442@yandex.ru>
2024-04-28 05:02:58 +0300

    add v

--- frontend/src/Components/Voting/Voting.jsx
+++ frontend/src/Components/Voting/Voting.jsx
@@ -5,16 +5,20 @@ import VoitingButton from '../VoitingButton/VoitingButton'
 export default function Voting ({ data }) {
 
     const districts = Object.keys(data)
-    console.log(districts)
+
+    function add_voice ({ prop }) {
+        console.log(prop.town)
+        // put('/${town}_1')
+    }
 
     return (
         <div className='voiting'>
             <h1 className='title'>Голосование</h1>
             {districts.map((district) => (
-                <div className='district'> 
+                <div key={district} className='district'> 
                     <h1>{district} округ</h1> 
                     <ul className='districts'>{Object.keys(data[district]).map((town) => (
-                        <li> {town} | Голоса: {data[district][town]} <VoitingButton/></li>))} 
+                        <li key={town}> {town} | Голоса: {data[district][town]} <VoitingButton onClick={() => put('/' + town + '_1')}/></li>))} 
                     </ul> 
                 </div>) )}
         </div>
